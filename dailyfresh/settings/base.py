313120309af4df733cc248dcb786dec35338876f
侯伟轩 <wxhou@yunjinginc.com>
2021-09-03 18:13:44 +0800

    update

--- dailyfresh/settings/base.py
+++ dailyfresh/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'drf_yasg',  # swagger
     'django_celery_results',  # celery结果后端
+    'corsheaders',
     'ckeditor',
     'ckeditor_uploader',
     'apps.user',
@@ -48,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
